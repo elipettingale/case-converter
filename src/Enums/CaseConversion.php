bd640cf6f8ded7f2129caddb75df6d14617eafe8
Elliot Pettingale <elipettingale@gmail.com>
2018-05-28 11:32:43 +0100

    add conversions enums

---
+++ src/Enums/CaseConversion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace EliPett\CaseConverter\Enums;
+
+class CaseConversion
+{
+    public const STRIP_SPECIAL_CHARS = 'STRIP_SPECIAL_CHARS';
+
+    public const LOWER_CASE = 'LOWER_CASE';
+    public const UPPER_CASE = 'UPPER_CASE';
+
+    public const TIGHT_CASE = 'TIGHT_CASE';
+
+    public const LOWER_SNAKE_CASE = 'LOWER_SNAKE_CASE';
+    public const UPPER_SNAKE_CASE = 'UPPER_SNAKE_CASE';
+
+    public const LOWER_CAMEL_CASE = 'LOWER_CAMEL_CASE';
+    public const UPPER_CAMEL_CASE = 'UPPER_CAMEL_CASE';
+
+    public const LOWER_HYPHEN_CASE = 'LOWER_HYPHEN_CASE';
+    public const UPPER_HYPHEN_CASE = 'UPPER_HYPHEN_CASE';
+}
